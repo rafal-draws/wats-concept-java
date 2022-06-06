15208632ef5a2daa788beaaf518299fa48973fe9
rafal-ftw <rafal.w.draws@gmail.com>
2022-06-06 22:39:29 +0200

    found it, created the scope of possible steps to be implemented

--- src/main/java/automationtool/services/ExecutorService.java
+++ src/main/java/automationtool/services/ExecutorService.java
@@ -33,25 +33,13 @@ public class ExecutorService {
         Scenario scenario = scenarioService.getScenario(scenarioId);
         HashMap<Integer, Object> steps = scenario.getScenarioAttributes();
         for (Map.Entry<Integer, Object> set : steps.entrySet()){
-            response.append(set.getValue());
+            interpretStep(String.valueOf(set.getValue()));
         }
 
-        //instructions to driver
         return String.valueOf(response);
     }
 
-    private void interpretStep(Object step){
-
-        // from scenario.getScenarioAttributes (which is a HashMap<Integer, Object>)
-        // we're only interested in value part, being the object of json syntax
-
-
-//        switch (step){
-//            case "GoToUrl":
-//                System.out.println("gotoUrl");
-//                break;
-//            case ""
-
-
+    private void interpretStep(String step){
+        System.out.println((step).replaceAll("[{}]", ""));
         }
 }
